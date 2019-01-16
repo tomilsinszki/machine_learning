f7e904d5fe62d2c66e214bd1907c6b62486740dd
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2019-01-16 22:46:17 +0100

    monthly periods, only active users, only active partners

--- PendingTransactionsReportCommand.php
+++ PendingTransactionsReportCommand.php
@@ -15,54 +15,61 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\DBAL\DBALException;
 
 class PendingTransactionsReportCommand extends ContainerAwareCommand
 {
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @var array
+     */
+    private $periods = array();
+
+    /**
+     * @var array
+     */
+    private $userIds = array();
+
     protected function configure() {
         $this->setName('rabattcorner:pending:transactions');
         $this->setDescription('Pending Transaction List');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        /** @var EntityManager $em */
-        $em = $this->getContainer()->get('doctrine')->getManager();
-
-        $periods = array();
+    private function loadMonthlyPeriods() {
+        $this->periods = array();
 
         $now = new \DateTime();
         $currentDay = new \DateTime();
         $currentDay->setDate(2017, 1, 1);
         $currentDay->setTime(0, 0, 0, 0);
 
+        $previousYear = 2016;
+        $previousMonth = 12;
+
         $shouldRun = true;
         while ($shouldRun) {
             $year = $currentDay->format("Y");
+            $month = $currentDay->format("n");
 
-            $weekNumber = ltrim($currentDay->format("W"), '0');
-            $previousWeekNumber = ($weekNumber-1);
+            $periodKey = "{$year}-{$month}";
+            $previousPeriodKey = "{$previousYear}-{$previousMonth}";
 
-            $periodKey = "{$year}-{$weekNumber}";
-            $previousPeriodKey = "{$year}-{$previousWeekNumber}";
-
-            $isNewPeriod = !isset($periods[$periodKey]);
-            $hasPreviousPeriod = isset($periods[$previousPeriodKey]['start']);
-
-            // TODO: hack fix
-            /*
-            if ($periodKey === '2018-1') {
-                $periods['2017-52'] = array(
-                    'start' => new \DateTime('2017-12-25 00:00:00'),
-                    'end' => new \DateTime('2018-01-01 00:00:00'),
-                );
-            }
-            */
+            $isNewPeriod = !isset($this->periods[$periodKey]);
+            $hasPreviousPeriod = isset($this->periods[$previousPeriodKey]['start']);
 
             if ($isNewPeriod) {
-                $periods[$periodKey] = ['start' => clone $currentDay];
+                $this->periods[$periodKey] = ['start' => clone $currentDay];
 
                 if ($hasPreviousPeriod) {
-                    $periods[$previousPeriodKey]['end'] = clone $currentDay;
+                    $this->periods[$previousPeriodKey]['end'] = clone $currentDay;
                 }
+
+                $previousYear = $year;
+                $previousMonth = $month;
             }
 
             $currentDay->modify('+1 day');
@@ -71,58 +78,99 @@ class PendingTransactionsReportCommand extends ContainerAwareCommand
             }
         }
 
-        array_shift($periods);
-        array_pop($periods);
+        array_pop($this->periods);
+    }
 
-        // TODO: hack fix
-        $periods['2017-51']['end'] = new \DateTime('2017-12-25 00:00:00');
+    private function loadWeeklyPeriods() {
+        $this->periods = array();
 
-        /*
-        $periods['2017-52'] = array(
-            'start' => new \DateTime('2017-12-25 00:00:00'),
-            'end' => new \DateTime('2018-01-01 00:00:00'),
-        );
-        */
+        $now = new \DateTime();
+        $currentDay = new \DateTime();
+        $currentDay->setDate(2017, 1, 2);
+        $currentDay->setTime(0, 0, 0, 0);
 
-        array_splice($periods, 51, 0, array(array(
-            'start' => new \DateTime('2017-12-25 00:00:00'),
-            'end' => new \DateTime('2018-01-01 00:00:00'),
-        )));
+        $previousYear = 2016;
+        $previousWeekNumber = 52;
 
-        /*
-        var_dump($periods['2017-51']);
-        var_dump($periods['2017-52']);
-        exit();
-        */
+        $shouldRun = true;
+        while ($shouldRun) {
+            $year = $currentDay->format("Y");
+            $weekNumber = ltrim($currentDay->format("W"), '0');
 
-        $periods['2018-52']['end'] = new \DateTime('2018-12-31 00:00:00');
+            $periodKey = "{$year}-{$weekNumber}";
+            $previousPeriodKey = "{$previousYear}-{$previousWeekNumber}";
 
-        //print_r($periods);exit();
+            $isNewPeriod = !isset($this->periods[$periodKey]);
+            $hasPreviousPeriod = isset($this->periods[$previousPeriodKey]['start']);
 
-        $statement = $em->getConnection()->prepare("SELECT id FROM account_user WHERE account_user.id AND account_user.locked = 0 ORDER BY id");
+            if ($isNewPeriod) {
+                $this->periods[$periodKey] = ['start' => clone $currentDay];
+
+                if ($hasPreviousPeriod) {
+                    $this->periods[$previousPeriodKey]['end'] = clone $currentDay;
+                }
+
+                $previousYear = $year;
+                $previousWeekNumber = $weekNumber;
+            }
+
+            $currentDay->modify('+1 day');
+            if ($now < $currentDay) {
+                $shouldRun = false;
+            }
+        }
+
+        array_pop($this->periods);
+    }
+
+    /**
+     * @throws DBALException
+     */
+    private function loadUserIds() {
+        $this->userIds = array();
+
+        $statement = $this->entityManager->getConnection()->prepare("SELECT id FROM account_user WHERE account_user.id AND account_user.locked = 0 AND account_user.enabled=1 ORDER BY id");
         $statement->execute();
         $userIdResults = $statement->fetchAll();
 
         foreach($userIdResults as $userIdResult) {
-            $userId = intval($userIdResult['id']);
+            $this->userIds[] = intval($userIdResult['id']);
+        }
+    }
 
-            $firstAndLastContactIdStatement = $em->getConnection()->prepare("SELECT MIN(contact_id) AS `first`, MAX(contact_id) AS `last` FROM user_contact WHERE user_id={$userId}");
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     * @throws DBALException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $this->entityManager = $this->getContainer()->get('doctrine')->getManager();
+
+        $this->loadMonthlyPeriods();
+        $this->loadUserIds();
+
+        foreach ($this->userIds as $userId) {
+            $firstAndLastContactIdStatement = $this->entityManager->getConnection()->prepare("SELECT MIN(contact_id) AS `first`, MAX(contact_id) AS `last` FROM user_contact WHERE user_id={$userId}");
             $firstAndLastContactIdStatement->execute();
             $firstAndLastContactResult = $firstAndLastContactIdStatement->fetchAll();
 
             $firstContactId = $firstAndLastContactResult[0]['first'];
             $lastContactId = $firstAndLastContactResult[0]['last'];
 
-            $signupDateStatement = $em->getConnection()->prepare("SELECT `time` FROM Contact WHERE id={$firstContactId}");
+            $signupDateStatement = $this->entityManager->getConnection()->prepare("SELECT `time` FROM Contact WHERE id={$firstContactId}");
             $signupDateStatement->execute();
             $signupDateString = $signupDateStatement->fetchColumn(0);
             $signupDateString = substr($signupDateString, 0, 10);
 
-            $contactDetailsStatement = $em->getConnection()->prepare("SELECT `title`, `postalcode` FROM Contact WHERE id={$lastContactId}");
+            $contactDetailsStatement = $this->entityManager->getConnection()->prepare("SELECT `title`, `postalcode`, `language`, `birthday` FROM Contact WHERE id={$lastContactId}");
             $contactDetailsStatement->execute();
             $contactDetailsResult = $contactDetailsStatement->fetchAll();
             $postalCode = $contactDetailsResult[0]['postalcode'];
             $gender = $contactDetailsResult[0]['title'];
+            $language = $contactDetailsResult[0]['language'];
+            $birthday = $contactDetailsResult[0]['birthday'];
+            $birthday = substr($birthday, 0, 10);
 
             if ($gender === 'male') {
                 $gender = '1';
@@ -132,16 +180,20 @@ class PendingTransactionsReportCommand extends ContainerAwareCommand
                 $gender = '';
             }
 
-            $partnersStatement = $em->getConnection()->prepare("SELECT id FROM Partner");
+            if ($language === 'de') {
+                $language = '1';
+            } elseif ($language === 'fr') {
+                $language = '2';
+            }
+
+            $partnersStatement = $this->entityManager->getConnection()->prepare("SELECT id FROM Partner WHERE status='active'");
             $partnersStatement->execute();
             $partnerIdResults = $partnersStatement->fetchAll();
 
             foreach ($partnerIdResults as $partnerIdResult) {
                 $partnerId = intval($partnerIdResult['id']);
 
-                foreach ($periods as $periodName => $period) {
-                    //var_dump($periodName);exit();
-
+                foreach ($this->periods as $periodName => $period) {
                     /** @var \DateTime $start */
                     $start = $period['start'];
 
@@ -151,81 +203,28 @@ class PendingTransactionsReportCommand extends ContainerAwareCommand
                     $startString = "{$start->format('Y-m-d H:i:s')}";
                     $endString = "{$end->format('Y-m-d H:i:s')}";
 
-                    // TODO: continue if signupDate is later than start of period: signupDateString < startString
-                    // $startString
-                    // $signupDateString
-
-                    /*
-                    $signupDateTime = new \DateTime("{$signupDateString} 00:00:00");
-                    var_dump($start);
-                    var_dump($signupDateTime);
-                    exit();
-                    */
-
                     $signupDateTime = new \DateTime("{$signupDateString} 00:00:00");
 
                     if ($end->getTimestamp() <= $signupDateTime->getTimestamp()) {
                         continue;
                     }
 
-                    $year = $start->format("Y");
-                    $weekNumber = ltrim($start->format("W"), '0');
-                    //echo("{$year}, {$weekNumber}\n");
-
-                    $transactionStatement = $em->getConnection()->prepare("SELECT SUM(programAmount) AS program_amount FROM cashback_transaction WHERE partner_id={$partnerId} AND user_id={$userId} AND '{$startString}'<=time AND time<='{$endString}'");
+                    $transactionStatement = $this->entityManager->getConnection()->prepare("SELECT SUM(programAmount) AS program_amount FROM cashback_transaction WHERE partner_id={$partnerId} AND user_id={$userId} AND '{$startString}'<=time AND time<='{$endString}'");
                     $transactionStatement->execute();
                     $transactionProgramAmountResults = $transactionStatement->fetchAll();
                     $transactionSumProgramAmount = empty($transactionProgramAmountResults[0]['program_amount']) ? 0.0 : $transactionProgramAmountResults[0]['program_amount'];
+                    $transactionSumProgramAmount = number_format($transactionSumProgramAmount, '2', '.', '');
 
-                    // TODO: add week number
-                    echo("userId,{$userId},partnerId,{$partnerId},year,{$year},week,{$weekNumber},start,{$start->format('Y-m-d')},end,{$end->format('Y-m-d')},sum,{$transactionSumProgramAmount},signup,{$signupDateString},postalcode,{$postalCode},gender,{$gender}\n");
+                    $previousVisitCountStatement = $this->entityManager->getConnection()->prepare("SELECT COUNT(DISTINCT id) AS visit_count FROM PartnerVisit WHERE user_id={$userId} AND partner_id={$partnerId} AND time<\"{$start->format('Y-m-d')}\"");
+                    $previousVisitCountStatement->execute();
+                    $previousVisitCountResults = $previousVisitCountStatement->fetchAll();
+                    $previousVisitCount = empty($previousVisitCountResults[0]['visit_count']) ? 0 : intval($previousVisitCountResults[0]['visit_count']);
+
+                    //echo("userId,{$userId},partnerId,{$partnerId},year,{$year},week,{$weekNumber},start,{$start->format('Y-m-d')},end,{$end->format('Y-m-d')},sum,{$transactionSumProgramAmount},signup,{$signupDateString},postalcode,{$postalCode},gender,{$gender}\n");
+
+                    echo("{$userId},{$signupDateString},{$postalCode},{$gender},{$language},{$birthday},{$partnerId},{$start->format('Y-m-d')},{$end->format('Y-m-d')},{$previousVisitCount},{$transactionSumProgramAmount}\n");
                 }
             }
         }
-
-
-
-
-
-
-        /*
-        // TODO: maybe we should also include locked users?
-        $statement = $em->getConnection()->prepare("SELECT id FROM account_user WHERE account_user.locked = 0 ORDER BY id");
-        $statement->execute();
-        $userIdResults = $statement->fetchAll();
-
-        $now = new \DateTime();
-
-        foreach($userIdResults as $userIdResult) {
-            $userId = intval($userIdResult['id']);
-
-            $currentDay = new \DateTime();
-            $currentDay->setDate(2018, 1, 1);
-            $currentDay->setTime(0, 0, 0, 0);
-
-            $shouldRun = true;
-            while ($shouldRun) {
-                // TODO: implement
-                echo("{$userId} {$currentDay->format('Y-m-d')} ({$currentDay->format("W")})\n");
-
-                $partnersStatement = $em->getConnection()->prepare("SELECT id FROM Partner");
-                $partnersStatement->execute();
-                $partnerIdResults = $partnersStatement->fetchAll();
-
-                foreach ($partnerIdResults as $partnerIdResult) {
-                    $partnerId = intval($partnerIdResult['id']);
-
-                    $transactionStatement = $em->getConnection()->prepare("SELECT * FROM cashback_transaction WHERE partner_id={$partnerId} AND user_id={$userId} AND '{$currentDay->format('Y-m-d')} 00:00:00'<=time AND time<='2018-09-16 23:59:59'");
-                    $transactionStatement->execute();
-                    $transactionIdResults = $transactionStatement->fetchAll();
-                }
-
-                $currentDay->modify('+1 day');
-                if ($now < $currentDay) {
-                    $shouldRun = false;
-                }
-            }
-        }
-        */
     }
 }
